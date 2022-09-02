b066ac6ee4564d1fec5d692bf34d7bd3fe291972
Wesley Wei <wesleytianqiwei@gmail.com>
2022-09-01 22:03:22 -0400

    user routes update

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,11 +2,10 @@ const router = require('express').Router();
 
 const users = ['Bob', 'Alex', 'Will', 'Tristan'];
 
-module.exports = (db) => {
-  // all routes will go here 
-  router.get('/', (req, res) => {
-      res.json(users);
-  });
 
-  return router;
-}
+router.get('/', (req, res) => {
+  res.json(users);
+});
+
+module.exports = router
+

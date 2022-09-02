09fe0c7658557851ba507156bf0d043460da2054
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-09-02 16:53:07 -0400

    get data from lyrics api

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const musicRouter = require('./routes/music');
+const lyricsRouter = require('./routes/lyrics');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/music', musicRouter);
+app.use('/lyrics', musicRouter);
 
 
 module.exports = app;
--- server/routes/music.js
+++ server/routes/music.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request')
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  request({
-    url: `https://api.deezer.com/search/track?q=MyheartWillGoOn`,
-    method: 'GET',
-  },
-    (error, response, body) => {
-      if (error) {
-        console.log(error)
-      } else {
-        res.send(body)
-      }
-  })
-})
+const request = require("request")
 
 
+router.get('/', async (req, res, next) => {
+  const title = 'My Heart Will Go On';
+  const artist = 'Celine Dion';
+    return new Promise((resolve, reject) => {
+      request({
+        url: `https://api.deezer.com/search/track?q=${title}${artist}`,
+        method: 'GET',
+      },
+        (error, response, body) => {
+          if (error) {
+            console.log(error)
+          } else {
+            return resolve(JSON.parse(body))
+          }
+        })
+  
+    }).then((data)=>{
+      res.send(data);
+    })
+});
 
 module.exports = router;

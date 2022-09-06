fb9fc612ebb49fad9fb406edee82cde2490d18c6
Wesley Wei <wesleytianqiwei@gmail.com>
2022-09-05 23:48:07 -0400

    loginin setup

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cookieSession = require('cookie-session')
+const bcrypt = require('bcryptjs');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,13 +19,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name:'session',
+  keys:['wesley', 'jenny']
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/music', musicRouter);
 app.use('/lyrics', lyricsRouter);
 app.use('/searchByLyrics', searchByLyricsRouter);
-
 
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,52 @@
+
 const express = require('express');
 const router = express.Router();
+const db = require('../configs/db.config');
+
+const getUsernameLookUp = (username) => {
+  const queryText = `Select * from users where user = $1`;
+  const params = [username];
+  db.query(queryText, params).then(data => {
+    if (data.rows.length < 1) {
+      return null;
+    }
+    return data.rows;
+  });
+};
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', (req, res) => {
+  res.status(200).send("ok");
+});
+
+router.post('/login',(req, res) => {
+  // console.log(req.body);
+});
+
+router.get('/signup', (req, res) => {
+  if (req.body.username === "" || req.body.password === "") {
+    res.status(400).end();
+  }
+  res.status(200).end();
+ 
+});
+
+router.post('/signup', (req,res) => {
+  if (req.body.username === "" || req.body.password === "") {
+    res.status(400).end();
+  }
+  if (!getUsernameLookUp(req.body.username)) {
+    console.log("ok");
+    res.json({error: "User exist"});
+  }
+   
+
+  
+ 
+  
 });
 
 module.exports = router;

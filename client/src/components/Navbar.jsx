3b1ec9db2c8d6289f3424e2ee81f075144f352ba
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-09-06 17:34:10 +0000

    merge 2 Sep.6

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,33 +1,8 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import SearchMusic from './SearchMusic';
-
-export default function Navbar() {
-
-  return (
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#">Navbar</a>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-          <SearchMusic/>
-=======
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { login, signup, favourite, recent } from '../redux/visualMode'
+import SearchMusic
+ from "./SearchMusic";
 const Navbar = () => {
   const { mode } = useSelector(state => state.userMode)
   const dispatch = useDispatch()
@@ -57,19 +32,14 @@ const Navbar = () => {
               <span className="nav-link" onClick={()=>dispatch(signup())}>Signup</span>
             </li>
           </ul>
-          <form className="d-flex" role="search" >
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
-          </form>
->>>>>>> master
+          <SearchMusic/>
+
         </div>
       </div>
     </nav>
   )
-<<<<<<< HEAD
-}
-=======
+
 }
 
 export default Navbar
->>>>>>> master
+
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,21 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import playerReducer from "./player";
 import microphoneReducer from "./microphone"
 import listenReducer from "./listener"
-<<<<<<< HEAD
 import musicDataReducer from "./musicData";
-=======
-import visualModeReducer from "./visualMode";
->>>>>>> master
+import visualModeReducer from "./visualMode"
 
 export default configureStore({
   reducer: { 
     player: playerReducer, 
     microphone: microphoneReducer,
     listen: listenReducer,
-<<<<<<< HEAD
-    musicData: musicDataReducer
-=======
+    musicData: musicDataReducer,
     userMode: visualModeReducer
->>>>>>> master
   }
 })

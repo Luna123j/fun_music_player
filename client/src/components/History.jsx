00727ccdca2d271aa5d44781f3b29be2fd4c0e5f
Wesley Wei <wesleytianqiwei@gmail.com>
2022-09-08 00:49:36 -0400

    history display

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,36 +1,45 @@
 import axios from "axios";
-import React, {useEffect }from "react";
+import React, {useEffect, useState }from "react";
 import { useCookies } from "react-cookie";
 import { useSelector,useDispatch} from 'react-redux';
 
 
 const History = () => {
   const dispatch = useDispatch();
+  const [historyList, setHistoryList] = useState([])
   const { currentSongContent } = useSelector(state => state.currentSongData)
   const [cookie] = useCookies()
   // const clickHandler=()=>{
   //   dispatch()
   // }
-
+    console.log(currentSongContent)
   useEffect(()=>{
-    console.log(currentSongContent[currentSongContent.length-1]);
-    axios.post('/history',{currentSong: currentSongContent[currentSongContent.length-1],username: cookie.username})
-    .then((res)=>{
-      console.log(res);
-    })
-  },[])
+    // console.log(currentSongContent[currentSongContent.length-1]);
+    if (currentSongContent.length !== 0) {
+        const senddata = setTimeout(()=>{
+          axios.post('/history',{currentSong: currentSongContent[currentSongContent.length-1],username: cookie.username})
+          .then((res)=>{
+            console.info("from backend", res);
+           setHistoryList(res.data.reverse())
+          })     
+
+        }, 1000)
+        return ()=> {
+          clearTimeout(senddata);
+        }
+    }
+  },[currentSongContent])
   
   
   return (
     <div>
       <h1>History</h1>
-      <article>
-        {currentSongContent.map((item) => {
-          return (
-            <p><button ><i className="fa-solid fa-play"></i></button><img src={item.cover}/>{item.title}{"            "}{item.artist}</p>
-          );
-        })}
-      </article>
+      { (historyList.length !== 0) &&
+        historyList.map((item)=>{
+          return (<div><img src={item.cover} alt={item.title} />{item.title}</div>)
+        })
+      }
+    
     </div>
 
   )
